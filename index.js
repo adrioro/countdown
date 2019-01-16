d62db3609905dc636964ebdb6f5a43c0c258b25b
adrioro <artistsvillage@me.com>
2019-01-16 11:35:44 -0500

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-var app = {}; //First we define an object. This is also a global variable.
+      var app = {}; //First we define an object. This is also a global variable.
 
-app.countdown = function(settings) {
-  console.log(settings);
-};
+      app.countdown = function (settings) {
+          var interval,
+              counter = 0,
+              //Start and end points.
+              startAt = 0,
+              endAt = 0;
+
+          if (settings === undefined) {
+              console.log("Please provide settings.");
+          } else {
+
+              if (settings.startAt === undefined || settings.endAt === undefined) {
+                  console.log("Start and end number are required.");
+              } else {
+                  /*The parseInt() function parses a string and returns an integer.*/
+                  startAt = parseInt(settings.startAt, 10);
+                  endAt = parseInt(settings.endAt, 10);
+
+                  if (!isNaN(startAt) && !isNaN(endAt)) {
+                      counter = startAt;
+
+                      interval = setInterval(function () {
+
+                          if (counter < endAt) {
+                              //Not required:
+                              counter = 0; //Settings go back to the beginning.
+                              startAt = 0;
+                              endAt = 0;
+
+                              console.log("Finished");
+                              clearInterval(interval);
+                          } else {
+                              console.log(counter);
+                              counter = counter - 1;
+                          }
+                      }, 1000);
+                  }
+              }
+          }
+      };
+
+      /* clearInterval() method to cancel the timer */
